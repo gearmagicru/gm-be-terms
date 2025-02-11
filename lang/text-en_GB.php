1de2f601cf16808d90e76469bdaae68c049e667f
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-11 14:34:47 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Terms',
+    '{description}' => 'Web application components terms',
+    '{permissions}' => [
+        'any'  => ['Full access', 'Configuring components terms']
+    ],
+
+    // Form
+    '{form.title}' => 'Add term',
+    '{form.titleTpl}' => 'Edit term "{name}"',
+    // Form: поля
+    'Module' => 'Module',
+    'Extension' => 'Module extension',
+    'Widget' => 'Widget',
+    'Plugin' => 'Plugin',
+
+    // Grid: контекстное меню записи
+    'Edit record' => 'Edit',
+    // Grid: столбцы
+    'Name' => 'Name',
+    'Component type' => 'Component type',
+    'Component ID' => 'Component ID'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Термины',
+    '{description}' => 'Термины компонентов веб-приложения',
+    '{permissions}' => [
+        'any'  => ['Полный доступ', 'Настройка терминов компонентов']
+    ],
+
+    // Form
+    '{form.title}' => 'Добавление термина',
+    '{form.titleTpl}' => 'Изменение термина "{name}"',
+    // Form: поля
+    'Module' => 'Модуль',
+    'Extension' => 'Расширение модуля',
+    'Widget' => 'Виджет',
+    'Plugin' => 'Плагин',
+
+    // Grid: контекстное меню записи
+    'Edit record' => 'Редактировать',
+    // Grid: столбцы
+    'Name' => 'Название',
+    'Component type' => 'Тип компонента',
+    'Component ID' => 'Идент. компонента'
+];
